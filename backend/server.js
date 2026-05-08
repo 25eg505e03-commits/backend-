90a841423a6a73ded30f639cb6a5f914c5d939ad
25eg505e03-commits <25eg505e03@anurag.edu.in>
2026-05-08 20:53:18 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL
 ];
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: true,
   credentials: true
 }));
 const port=process.env.PORT||4000
